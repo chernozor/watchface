0768720042a746d50fb0eb4bfb953aa83c2ad209
chernozor <ghbdtnjc1>
2017-12-04 20:17:39 +0300

    1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # watchface
+Tizen Project 1.0
 Simple Watchface for Tizen 1.0 API ver.2.3.2
---
+++ inc/minimalwatch.h
@@ -0,0 +1,18 @@
+#ifndef __minimalwatch_H__
+#define __minimalwatch_H__
+
+#include <watch_app.h>
+#include <watch_app_efl.h>
+#include <Elementary.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "minimalwatch"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.minimalwatch"
+#endif
+
+#endif /* __minimalwatch_H__ */
